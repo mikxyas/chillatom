e4882a3b3b2df4efbe5b88947cd77f9e73724f7a
Mikiyas <mymikiaa11@gmail.com>
2021-09-12 18:25:44 +0300

    added privacy policy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,129 @@
+export default function Privacy() {
+    return(
+        <div className='flex items-center p-2 justify-center '>
+            <div style={{height:'650px'}} className='p-3 mt-3 font-mono text-black flex-col bg-white w-full md:w-2/5  rounded-lg overflow-y-scroll'>
+                <h1 className='mt-1 text-3xl'>Privacy</h1>
+                <p className='mt-3'>Effective date: September 10, 2021</p>
+                <p className='mt-3'>
+                    Chillatom ("us", "we", or "our") operates the https://chillatom.com website (hereinafter referred to as the "Service").
+                </p>
+                <p className='mt-3'>
+                    This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data.
+                </p>
+                <p className='mt-3'>
+                We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, the terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from https://chillatom.com
+                </p>
+                <p className='mt-5 text-3xl'>
+                Information Collection And Use
+                </p>
+                <p className='mt-3'>
+                We collect several different types of information for various purposes to provide and improve our Service to you.
+                </p>
+                <p className='mt-3 text-2xl'>Types of Data Collected</p>
+                <p className='mt-3 text-xl'>Personal Data</p>
+                <p className='mt-3'>While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to identify you. Personally identifiable information may include, but is not limited to: Cookies and Usage Data</p>
+                <p className='mt-3 text-xl'>Usage Data</p>
+                <p className='mt-3'>We may also collect information on how the Service is accessed and used ("Usage Data"). This Usage Data may include information such as your computer's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</p>
+                <p className='mt-3 text-xl'>Tracking and Cookies Data</p>
+                <p className='mt-3'> 
+                    We use cookies and similar tracking technologies to track the activity on our Service and hold certain information.
+                    Cookies are files with small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyze our Service.
+                    You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service. You can learn more how to manage cookies in the <a className='text-blue-600 hover:underline' href='https://privacypolicies.com/blog/how-to-delete-cookies/'>Browser Cookies Guide.</a>
+                </p>
+                <p className='mt-3'>
+                Examples of Cookies we use:
+                </p>
+                <p className='mt-2 ml-5'>
+                Session Cookies. We use Session Cookies to operate our Service.
+                </p>
+                <p className='mt-2 ml-5'>
+                Preference Cookies. We use Preference Cookies to remember your preferences and various settings.
+                </p>
+                <p className='mt-2 ml-5'>
+                Security Cookies. We use Security Cookies for security purposes.
+                </p>
+                <p className='mt-3 text-2xl'>Usage of Data</p>
+                <p className='mt-3'>
+                Pomofocus uses the collected data for various purposes:
+                </p>
+                <p className='mt-3 ml-5'>
+                To provide and maintain the Service
+                </p>
+                <p className='mt-2 ml-5'>
+                To notify you about changes to our Service
+                </p>
+                <p className='mt-2 ml-5'>
+                To allow you to participate in interactive features of our Service when you choose to do so
+                </p>
+                <p className='mt-2 ml-5'>
+                To provide customer care and support
+                </p>
+                <p className='mt-2 ml-5'>
+                To provide analysis or valuable information so that we can improve the Service
+                </p>
+                <p className='mt-2 ml-5'>
+                To monitor the usage of the Service
+                </p>
+                <p className='mt-2 ml-5'>
+                To monitor the usage of the Service
+                </p>
+                <p className='mt-2 ml-5'>
+                To detect, prevent and address technical issues
+                </p>
+                <p className='mt-3 text-2xl'>
+                Transfer Of Data
+                </p>
+                <p className='mt-3 '>
+                Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.
+                </p>
+                <p className='mt-3 '>
+                If you are located outside Japan and choose to provide information to us, please note that we transfer the data, including Personal Data, to Japan and process it there.
+                </p>
+                <p className='mt-3 '>
+                Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.
+                </p>
+                <p className='mt-3'>
+                Chillatom will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.
+                </p>
+                <p className='mt-3 text-2xl'>
+                Disclosure Of Data
+                </p>
+                <p className='mt-3 text-xl'>
+                Legal Requirements
+                </p>
+                <p className='mt-3 '>
+                Chillatom may disclose your Personal Data in the good faith belief that such action is necessary to:
+                </p>
+                <p className='mt-2 ml-5'>
+                To comply with a legal obligation
+                </p> 
+                <p className='mt-2 ml-5'>
+                To protect and defend the rights or property of Pomofocus
+                </p>
+                <p className='mt-2 ml-5'>
+                To prevent or investigate possible wrongdoing in connection with the Service
+                </p>
+                <p className='mt-2 ml-5'>
+                To protect the personal safety of users of the Service or the public
+                </p>
+                <p className='mt-2 ml-5'>
+                To protect against legal liability
+                </p>
+                <p className='text-2xl mt-5'>
+                    Security Of Data
+                </p>
+                <p className='mt-3'>The security of your data is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.</p>
+                <p className='mt-3 text-2xl'>Service Providers</p>
+                <p className='mt-3'>We may employ third party companies and individuals to facilitate our Service ("Service Providers"), to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.</p>
+                <p className='mt-3'>These third parties have access to your Personal Data only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.</p>
+                <p className='mt-3 text-2xl'> Changes To This Privacy Policy</p>
+                <p className='mt-3'>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+                <p className='mt-3'>We will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective and update the "effective date" at the top of this Privacy Policy.</p>
+                <p className='mt-3'>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+                <p className='text-2xl mt-3'>Contact Us</p>
+                <p className="mt-3">If you have any questions about this Privacy Policy, please contact us by sending email to: mikiyas.dev@gmail.com</p>
+            </div>
+        </div>
+        
+    )
+}
